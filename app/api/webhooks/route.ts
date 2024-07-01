b853fe9cc951e492c2177c893c78e41c7914a396
Adham Nasser <adhamxiii22@gmail.com>
2024-07-01 05:19:36 +0300

    fix some bugs 🐞

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -5,11 +5,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 import getRawBody from 'raw-body';
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
 export const POST = async (req: any) => {
   try {
